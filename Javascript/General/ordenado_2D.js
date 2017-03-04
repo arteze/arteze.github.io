61dcc4e562632482756c0b8d194bcca3f38a46c0
Emiliano Ezequiel Parenti <passymas@gmail.com>
2017-03-04 11:42:21 -0300

    Agregando código de ordenar matrices 2D.

---
+++ Javascript/General/ordenado_2D.js
@@ -0,0 +1,36 @@
+﻿function criterio_números(columnas)
+{
+	var devuelve="\t"+"var columnas="+JSON.stringify(columnas)+"\n\t"+
+		"for(var i=0;i<columnas.length;i++){"+"\n\t\t"+
+			"var columna=columnas[i][0]"+"\n\t\t"+
+			"var menos=1-2*columnas[i][1]"+"\n\t\t"+
+			"if(a[columna]>b[columna]){return +menos}"+"\n\t\t"+
+			"if(a[columna]<b[columna]){return -menos}"+"\n\t"+
+		"};return 0"
+	return Function("a","b",devuelve)
+}
+function criterio_secuencias(columnas)
+{
+	var devuelve="\t"+"var columnas="+JSON.stringify(columnas)+"\n\t"+
+		"for(var i=0;i<columnas.length;i++){"+"\n\t\t"+
+			"var columna=columnas[i][0]"+"\n\t\t"+
+			"var menos=1-2*columnas[i][1]"+"\n\t\t"+
+			"if(a[columna].length>b[columna].length){return +menos}"+"\n\t\t"+
+			"if(a[columna].length<b[columna].length){return -menos}"+"\n\t"+
+			"if(a[columna]>b[columna]){return +menos}"+"\n\t\t"+
+			"if(a[columna]<b[columna]){return -menos}"+"\n\t"+
+		"};return 0"
+	return Function("a","b",devuelve)
+}
+function ordenar(matriz,columnas,compara)
+{
+	return matriz.slice(0).sort(compara)
+}
+function ordenar_números(matriz,columnas)
+{
+	return ordenar(matriz,columnas, criterio_números(columnas) )
+}
+function ordenar_secuencias(matriz,columnas)
+{
+	return ordenar(matriz,columnas, criterio_secuencias(columnas) )
+}
