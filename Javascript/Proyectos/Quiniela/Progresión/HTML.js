42cb17bd096fdf0dbaf2288c1f9ea4e1bdec32fd
Emiliano Ezequiel Parenti <passymas@gmail.com>
2017-09-26 12:00:23 -0300

    Agregando proyecto de Quiniela: Progresiones.

---
+++ Javascript/Proyectos/Quiniela/Progresión/HTML.js
@@ -0,0 +1,35 @@
+﻿function formatear(resultado,mensaje_extra)
+{
+	var devuelve=""
+	devuelve+="<table border=1>"
+	//if(mensaje_extra)
+	{
+		devuelve+="<tr>"
+		for(var i in mensaje_extra)
+		{
+			devuelve+="<td align=center>"+mensaje_extra[i]+"</td>"
+		}
+		devuelve+="</tr>"
+	}
+	for(var i in resultado)
+	{
+		devuelve+="<tr>"
+		for(var j in resultado[i])
+		{
+			devuelve+="<td align=center>"
+			devuelve+=resultado[i][j]
+			devuelve+="</td>"
+		}
+		devuelve+="</tr>"
+	}
+	devuelve+="</table>"
+	return devuelve
+}
+function progresión()
+{
+	var dinero=tengo.value
+	var gastado=eval("["+gastos.value+"]")
+	var resultado=pérdidas_consecutivas(dinero,gastado)
+	var mensaje_extra="ABCDEFG"
+	res.innerHTML=formatear(resultado,mensaje_extra)
+}
---
+++ Javascript/Proyectos/Quiniela/Progresión/programa.js
@@ -0,0 +1,71 @@
+﻿function calcular_gasto(cantidad_números,dinero,gastado)
+{
+	return [
+			cantidad_números,
+			dinero,
+			cantidad_números*dinero,
+			7*dinero,
+			dinero*(7-cantidad_números),
+			dinero*(7-cantidad_números)-gastado
+		]
+}
+function sumar_array(array)
+{
+	var devuelve=0
+	for(var i in array)
+	{
+		devuelve+=array[i]
+	}
+	return devuelve
+}
+function cuánto_jugar(dinero,jugadas)
+{
+	var gastado=sumar_array(jugadas)
+	var i=Math.floor(dinero/gastado)
+	var array=[]
+	var actual=[]
+	var alcanza
+	for(var i=1;i<7;i++)
+	{
+		alcanza=Math.floor(dinero/i)
+		var diferencia=alcanza*(7-i)-gastado
+		if(diferencia<0){i--;break}
+		actual=calcular_gasto(i,alcanza,gastado)
+	}
+	var i=actual[0]
+	var jugar=actual[1]
+	var copia=actual
+	while(true)
+	{
+		actual=calcular_gasto(i,jugar,gastado)
+		if(
+			actual[5]<0
+			| actual[0]==undefined
+			| isNaN(actual[1])
+		){break;}
+		copia=actual
+		jugar--
+	}
+	return copia
+}
+function pérdidas_consecutivas(dinero,jugadas)
+{
+	var array=[]
+	for(var i=0;i<18;i++)
+	{
+		var actual=cuánto_jugar(dinero,jugadas)
+		dinero-=actual[2]
+		jugadas.push(actual[2])
+		array.push(actual)
+		actual.push("$"+dinero)
+		//console.log(actual)
+		if(
+			actual[0]==0
+			| actual[1]==0
+			| actual[0]==undefined
+			| actual[0]=="$NaN"
+		){break;}
+	}
+	array.pop()
+	return array
+}
