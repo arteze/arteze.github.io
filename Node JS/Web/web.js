0b484bad3b576b729d36d8740d5fd8092383b1e0
Emiliano Ezequiel Parenti <passymas@gmail.com>
2018-06-27 20:01:58 -0300

    Servidor web simple.

---
+++ Node JS/Web/web.js
@@ -0,0 +1,21 @@
+﻿// http://190.193.163.21:82/ejemplo
+
+var http = require('http')
+var url=require('url');
+var hostname = '192.168.3.245'
+var port = 82
+var server=http.createServer(function(req,res){
+    var pathname=url.parse(req.url).pathname;
+	res.end(
+		"<h1>"+(pathname=='/ejemplo'?
+			"Correcto: "
+		:
+			"Error 404: "
+		)+((1000+Math.floor(Math.random()*1000))+"").slice(1)
+		+"</h1>"
+	)
+
+})
+server.listen(port, hostname, () => {
+  console.log("Ejecutando servidor http://"+hostname+":"+port)
+})
