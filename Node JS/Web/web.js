b9cc1eb04bd5630fe42c85612c1d6ab3403ab6d1
Emiliano Ezequiel Parenti <passymas@gmail.com>
2018-12-02 19:09:06 -0300

    Web

--- Node JS/Web/web.js
+++ Node JS/Web/web.js
@@ -1,26 +1,37 @@
 ﻿// https://stackoverflow.com/questions/46141175/running-node-js-server-on-localhost
 // http://190.193.163.21:82/ejemplo
 
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+
+function dos_dígitos(entero)
+{
+	return ("00"+entero).slice(-2)
+}
+
+function mostrar_fecha(req)
+{
+	var fecha = new Date()
+	var día = dos_dígitos(fecha.getDate())
+	var horas = dos_dígitos(fecha.getHours())
+	var minutos = dos_dígitos(fecha.getMinutes())
+	var segundos = dos_dígitos(fecha.getSeconds())
+	console.log(día+" "+horas+":"+minutos+":"+segundos,req.url)
+}
 
 http.createServer(function (req, res) {
-    const filePath = path.join(__dirname, 'index.html');
-    const stat = fs.statSync(filePath);
+    var filePath = path.join(__dirname, 'index.html')
+    var stat = fs.statSync(filePath)
 
     res.writeHead(200, {
 		'Content-Type': 'text/html',
 		'Content-Length': stat.size
-    });
-
-	var dos_dígitos = x=>("00"+x).slice(-2)
-    var stream = fs.createReadStream(filePath);
-    stream.pipe(res);
-	var fecha = new Date()
-	var horas = dos_dígitos(fecha.getHours())
-	var minutos = dos_dígitos(fecha.getMinutes())
-	var segundos = dos_dígitos(fecha.getSeconds())
-	console.log(horas,minutos,segundos)
+    })
 	
-}).listen(8080);
+    var stream = fs.createReadStream(filePath)
+    stream.pipe(res)
+	
+	mostrar_fecha(req)
+
+}).listen(8081);
