bb34b8ff9ee613dccef70fd3f0f3902b16986a8a
ArtEze <passymas@gmail.com>
2019-07-21 02:50:06 -0300

    Cambiando nombre Node.js por NodeJS
    Cambiando nombre Node.js por NodeJS

--- Lenguajes/Node.JS/BotsVsBots/NodeChatovod_BotsVsBots_2.js
+++
@@ -1,5 +0,0 @@
-/*
-node "/eze/Node/BotsVsBots/NodeChatovod_BotsVsBots_2.js" nekonwn 0
-*/
-
-;
--- Lenguajes/Node.JS/BotsVsBots/gedit_proyecto.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-setsid gedit "./node_modules/http_otecald/index.js" "./NodeChatovod_BotsVsBots_1.js" "./NodeChatovod_BotsVsBots_2.js" "./gedit_proyecto.sh" &
-echo "Abriendo proyecto"
-sleep 5s
-
