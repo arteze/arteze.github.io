9323d89e6994cb9389c2ce87d49804b5dcc0ff41
ArtEze <passymas@gmail.com>
2019-12-01 23:18:26 -0300

    Mostrando argumentos por consola
    Mostrando argumentos por consola.

--- lenguaje/cint/naturales.c
+++ lenguaje/cint/naturales.c
@@ -1,15 +1,12 @@
-char* nuevo_texto(){
-	return "";
+char* nuevo_texto()         {return "";}
+char* nuevo_salto_de_linea(){return "\n";}
+char* nuevo_espacio()       {return "\x20";}
+char* nuevo_natural()       {return "";}
+
+char* natural_desde_texto(char* texto){
+	return texto;
 }
-char* nuevo_natural(){
-	return "";
-}
-char* nuevo_salto_de_linea(){
-	return "\n";
-}
-char* nuevo_espacio(){
-	return " ";
-}
+
 void mostrar_string(char* string){
 	printf("%s",string);
 	return;
@@ -40,30 +37,34 @@ void mostrar_natural(char* string){
 }
 char* obtener_longitud_como_string(char* string){
 	long longitud = obtener_longitud_de_string_como_long(string);
-	char* long_string = malloc( sizeof(size_t) + 1 );
-	for(int i=7;i>=0;--i){
-		long_string[i] = '0'+longitud%10;
+	int longitud_longitud = sizeof(longitud);
+	char* long_string = malloc( longitud_longitud + 1 );
+	for(int i=longitud_longitud;i>0;--i){
+		long_string[i-1] = '0'+longitud%10;
 		longitud /= 10;
 	}
 	long_string[8] = '\x00';
 	return long_string;
 }
-int main()
+int main(int argc, char *argv[])
 {
+	mostrar_string("Texto: '");
 	char* cadena = nuevo_texto();
 	char* salto_de_linea = nuevo_salto_de_linea();
 	mostrar_string(cadena);
-	mostrar_string( salto_de_linea );
+	mostrar_string( "'\n" );
+	for(int i=1;i<argc;++i){
+		char* actual = argv[i];
+		mostrar_string("Entero: ");
+		char* natural = natural_desde_texto(actual);
+		mostrar_natural(natural);
+		mostrar_string( salto_de_linea );
 
-	mostrar_string("Entero: ");
-	char* natural = nuevo_natural();
-	mostrar_natural(natural);
-	mostrar_string( salto_de_linea );
-
-	char* longitud_de_natural = obtener_longitud_como_string(natural);	
-	mostrar_string("Longitud de natural: ");
-	mostrar_natural(longitud_de_natural);
-	mostrar_string( salto_de_linea );
+		char* longitud_de_natural = obtener_longitud_como_string(natural);	
+		mostrar_string("Longitud de natural: ");
+		mostrar_natural(longitud_de_natural);
+		mostrar_string( salto_de_linea );
+	}
 	return 0;	
 }
 
