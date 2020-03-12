c75cb0ae82e3f7615054533c964d05c640a5a319
Emiliano Ezequiel Parenti <passymas@gmail.com>
2020-03-12 03:58:48 -0300

    Transcripciones: Agrego argumento de carpeta.
    Transcripciones: Agrego el parámetro de argumentos, el cual tiene el parámetro del nombre de la carpeta relativa a donde se guardará la transcripción.

--- lenguaje/node_js/descargar_transcripciones/index.js
+++ lenguaje/node_js/descargar_transcripciones/index.js
@@ -1,5 +1,5 @@
 
-function descargar_https_asíncrono(url,mostrar_resultado,callback){
+function descargar_https_asíncrono(url,mostrar_resultado,callback,argumentos){
 	var espera = 5*1000
 	setTimeout(function(){
 		https.get(url,function(recurso){
@@ -10,7 +10,7 @@ function descargar_https_asíncrono(url,mostrar_resultado,callback){
 			})
 			recurso.on("end", function(){
 				mostrar_resultado && console.log(resultado)
-				callback(resultado,recurso)
+				callback(resultado,recurso,argumentos)
 			})	
 		})	
 	},Math.floor(Math.random()*espera+espera))
@@ -55,14 +55,16 @@ function obtener_fecha_corta(url){
 	return url.split("/").slice(-1-3,-1).map(x=>(("0"+x).slice(-2))).join("")
 }
 
-function procesar_resultado(resultado,recurso){
+function procesar_resultado(resultado,recurso,argumentos){
 	var día_encontrado = true
 	var tipo = "next"
 
 	var ruta_actual = recurso.socket._httpMessage.path
 	//console.log( "Descargado: " , ruta_actual )
 
-	var ruta_archivo = "./chat_volcado/" + obtener_fecha_corta(ruta_actual) + ".html"
+	var carpeta = argumentos.carpeta
+
+	var ruta_archivo = "./" + carpeta + "/" + obtener_fecha_corta(ruta_actual) + ".html"
 	guardar_archivo(ruta_archivo,resultado)
 
 	if( !encuentra_día(resultado,tipo) ){
@@ -77,32 +79,39 @@ function procesar_resultado(resultado,recurso){
 		var relativo = obtener_día_relativo(resultado,tipo)
 		var enlace_con_horas = obtener_origen() + relativo + obtener_día_completo()
 		console.log(enlace_con_horas)
-		descargar_día(enlace_con_horas)
+		descargar_día(enlace_con_horas,argumentos)
 	}
 }
-function descargar_día(url){
-	descargar_https_asíncrono(url,false,procesar_resultado)
+function descargar_día(url,argumentos){
+	descargar_https_asíncrono(url,false,procesar_resultado,argumentos)
 }
-function callback_transcripción_final(resultado){
+function callback_transcripción_final(resultado,argumentos){
 	//console.log(resultado.slice(0,2000))
 	var tipo = "first"
 	if( encuentra_día(resultado,tipo) ){
 		var relativo = obtener_día_relativo(resultado,tipo)
 		var url = obtener_origen() + relativo + obtener_día_completo()
 		console.log(url)
-		descargar_día(url)
+		descargar_día(url,argumentos)
 	}else{
 		console.log("Día no encontrado.")
 	}
 }
-function descargar_desde_día_inicial(sala){
+function descargar_desde_día_inicial(sala,carpeta){
 	var relativo = "/transcript/" + sala
 	var ruta = obtener_origen() + relativo
-	descargar_https_asíncrono(ruta,false,callback_transcripción_final)
+	var argumentos = {
+		carpeta: carpeta
+	}
+	function callback(x){
+		return callback_transcripción_final(x,argumentos)
+	}
+	descargar_https_asíncrono(ruta,false,callback,argumentos)
 }
 function comenzar_descarga(){
 	var sala = 30872
-	descargar_desde_día_inicial(sala)
+	var carpeta = "chat_soes_volcado"
+	descargar_desde_día_inicial(sala,carpeta)
 }
 
 function exportar(){
@@ -110,6 +119,7 @@ function exportar(){
 	var exportado = [
 		"descargar_https_asíncrono"
 		, "descargar_día"
+		, "descargar_desde_día_inicial"
 		, "comenzar_descarga"
 	].map(function(x){
 		objeto[x] = eval(x)
@@ -117,4 +127,3 @@ function exportar(){
 	return objeto
 }
 module.exports = exportar()
-
