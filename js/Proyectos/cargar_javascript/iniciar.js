8864b9d7d5e87ea4329fe2fce314acaff0729f83
ArtEze <passymas@gmail.com>
2020-03-23 14:57:53 -0300

    Update iniciar.js

--- js/Proyectos/cargar_javascript/iniciar.js
+++ js/Proyectos/cargar_javascript/iniciar.js
@@ -1,16 +1,32 @@
-function iniciar(callback,mostrar_error){
+function iniciar(callback,puede_depurar){
 	var iniciado = false
+	var contador = 0
+	var errores = []
 	var intervalo = setInterval(function(){
 		try{
 			callback()
 			iniciado = true
 		}catch(e){
-			if(mostrar_error){
+			if(puede_depurar){
 				console.log(e)
+			}
+			errores.push(e)
+			++contador
+			if(contador==1000){
+				clearInterval(intervalo)
+				var depurado = (function mostrar_errores(){
+					if(errores.length>0){
+						console.log(errores.shift())
+						setTimeout(mostrar_errores,50)
+					}
+				})()
 			}
 		}
 		if(iniciado){
 			clearInterval(intervalo)
+			if(puede_depurar){
+				console.log(iniciado,contador)
+			}
 		}
 	})
 	return intervalo
