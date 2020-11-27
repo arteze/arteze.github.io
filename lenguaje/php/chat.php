ed09eb27d7fa7d98c8b620b78fa6032eae5587b6
ArtEze <passymas@gmail.com>
2020-11-27 07:17:31 -0300

    Create chat.php

---
+++ lenguaje/php/chat.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Chat</title>
+		<link rel="stylesheet" href="chat/estilo.css" />
+		<script src="chat/programa.js" ></script>
+	</head>
+	<body>
+		<div class="chat_fuera">
+			<div class="columna_chat_a" >
+				<a>chat</a>
+			</div>
+			<div class="chat" >
+			</div>
+		</div>
+		<div class="texto">
+			<table>
+				<tr>
+					<td class="columna_alias_a" >
+						<a>Alias: </a>
+					</td>
+					<td class="columna_alias_b" >
+						<input/>
+					</td>
+					<td class="columna_textarea" >
+						<textarea></textarea>
+					</td>
+					<td class="columna_enviar" >
+						<button>Enviar</button>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
